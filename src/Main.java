651e8ec0ce42c6e4e23a08cd8af1453e9bcf77d2
c0mpl3x <marszemlya2312@gmail.com>
2023-09-14 11:25:21 +0300

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //Task 1
+        long l = 123456;
+        double d1, d2;
+        float f = 14.75f;
+        char c1 = '0';
+    }
+}
